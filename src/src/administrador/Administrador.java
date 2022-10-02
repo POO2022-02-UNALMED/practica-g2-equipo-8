9521eb65770587fa72a0342fddec71ba8d102ad5
Shanic0120 <juazapataca@unal.edu.co>
2022-10-02 16:45:54 -0500

    Update Administrador.java

--- src/src/administrador/Administrador.java
+++ src/src/administrador/Administrador.java
@@ -37,7 +37,25 @@ public class Administrador {
 				+ "4. Administrar finanzas.\n" + "5. Administrar vuelos y aviones.\n\n" + "6. Finalizar programa.\n");
 		Scanner entrada = new Scanner(System.in);
 		int option = entrada.nextInt();
-		System.out.println(option);
+		switch (option) {
+		case 1: {
+			break;
+		}
+		case 2: {
+			break;
+		}
+		case 3: {
+			break;
+		}
+		case 4: {
+			break;
+		}
+		case 5: {
+			break;
+		}
+		case 6: {
+			break;
+		}
+		}
 	}
-
 }
