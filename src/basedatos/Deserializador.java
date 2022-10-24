de31b6e06178c1040d6d8b62aef3b1b6d384857d
Shanic0120 <juazapataca@unal.edu.co>
2022-10-24 11:52:33 -0500

    correccion

--- src/basedatos/Deserializador.java
+++ src/basedatos/Deserializador.java
@@ -22,7 +22,7 @@ public class Deserializador {
 			aeropuerto.setEmpleados(e.getEmpleados());
 			aeropuerto.setNombre(e.getNombre());
 			aeropuerto.setPasajeros(e.getPasajeros());
-			//aeropuerto.setSalas(e.getSalas());
+			// aeropuerto.setSalas(e.getSalas());
 			aeropuerto.setVuelos(e.getVuelos());
 			aeropuerto.setTransaccionesKeys(e.getTransaccionesKeys());
 			aeropuerto.setTransaccionesValues(e.getTransaccionesValues());
