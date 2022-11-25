688ba18d5d6becee253653ad705b1ccce373d0eb
Juan Camilo Molina <jmolinaro@unal.edu.co>
2022-11-25 18:32:05 -0500

    Create fieldFrame.py

---
+++ python/GUI/fieldFrame.py
@@ -0,0 +1,53 @@
+from tkinter import *
+
+class FieldFrame(Frame):
+    
+    def __init__(self, ventana, tituloCriterios = "", criterios = None, tituloValores = "", valores = None, habilitado = None):
+        super().__init__(ventana)
+        self._tituloCriterios = tituloCriterios
+        self._criterios = criterios
+        self._tituloValores = tituloValores
+        self._valores = valores
+        self._habilitado = habilitado
+
+        # Lista de elementos
+        self._elementos = []
+
+        # Crear y colocar titulo de los criterios
+        labelTituloCriterios = Label(self, text = tituloCriterios, font= ("Courier", 14))
+        labelTituloCriterios.grid(column=0, row=0, padx = (5,5), pady = (5,5))
+
+        # Crear y colocar titulo de los valores
+        labelTituloValores = Label(self, text = tituloValores, font= ("Courier", 14))
+        labelTituloValores.grid(column=1, row=0, padx = (5,5), pady = (5,5))
+
+        # Crear cada uno de los criterios
+        for  i in range(len(criterios)):
+            # Crear y colocar nombre de cada criterio
+            labelCriterio = Label(self, text = criterios[i], font = ("Verdana", 12))
+            labelCriterio.grid(column=0, row=i+1, padx = (5,5), pady = (5,5))
+
+            # Crear y colocar entrada de cada criterio
+            entryValor = Entry(self, font = ("Verdana", 12))
+            entryValor.grid(column=1, row=i+1, padx = (5,5), pady = (5,5))
+
+            # Colocar el valor inicial si lo hay
+            if valores is not None:
+                entryValor.insert(0, valores[i])
+
+            # Si el campo es no-editable, deshabilitarlo
+            if habilitado is not None and not habilitado[i]:
+                entryValor.configure(state = DISABLED)
+            
+            # Anadir a la lista de elementos
+            self._elementos.append(entryValor)
+
+    # GetValue
+    # criterio: El criterio cuyo valor se quiere obtener
+
+    def getValue(self, criterio):
+        indice = self._criterios.index(criterio)
+        return self._elementos[indice].get()
+
+    def crearBotones(self, comando1):
+        aceptar = Button(self, text="Aceptar", font = ("Courier", 12), fg = "white", command=comando1).grid(pady = 50, column = 0, row = len(self._criterios)+1)
