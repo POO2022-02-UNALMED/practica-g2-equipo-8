19071060244e1685a2c248952cdd66a7dbfb85cc
Juan Camilo Molina <jmolinaro@unal.edu.co>
2022-11-26 22:15:45 -0500

    reservaDeVuelo WIP
    Para ir limpiando la pantalla agregar todos los widgets que se creen a widgetsActuales y llamar en cada metodo a borrarElementos al principio para limpiar antes de colocar otros widgets

--- python/GUI/ventanaPrincipal.py
+++ python/GUI/ventanaPrincipal.py
@@ -12,7 +12,9 @@ class VentanaUsuario(Tk):
         self.geometry("1280x720")
         self.ventanaInicio = None
         self.iconbitmap('./imagenes/icono.ico')
-        
+        self.widgetsActuales = []
+
+        #Funciones
         def prueba():
             pass
 
@@ -22,17 +24,79 @@ class VentanaUsuario(Tk):
         def info():
             messagebox.showinfo("Desarrolladores del aplicativo","Maria Camila Zapata Arrubla\nJuan Camilo Molina Roncancio\nJuan Jose Zapata Cadavid\nNombre4")
 
+        def borrarElementos():
+            for i in self.widgetsActuales:
+                i.forget()
+
+        #Pantallas
+        def pantallaPrincipal():
+            borrarElementos()
+            self.lp = Label(self.fp,text= "Pantalla principal", font = ("Courier", 12),height=2, bg="gray80")
+            self.lp.pack()
+            self.ld = Label(self.fd, text = "Descripción de funcionalidades", font = ("Courier", 10))
+            self.ld.pack()
+            descripcion = '''Bienvenidos a la aplicacion de gestion y modificacion de aeropuerto, aqui podra realizar diferentes funciones
+            relacionadas a la administracion del aeropuerto.
+
+             En la parte superior en el menu de procesos y consultas se listan Las principales funcionalidades, las cuales son:
+
+
+            1. Reserva de vuelo
+            En esta opcion esta disponible lo necesario para agendar un vuelo a determinado pasajero, se muestran en pantalla
+            todos los vuelos disponibles que se dirijan hacia la ciudad de destino deseada, luego se muestran los diferentes
+            vuelos disponibles. En el mismo apartado se implementa un pequeño formulario de datos personales para la
+            identificación del pasajero y así mismo la cantidad de equipajes que desea transportar. Tambien se le da la
+            posibilidad de elegir el asiento que desea, los cuales están separados por turista, ejecutiva y primera clase, 
+            esta elección influirá directamente en el costo del tiquete.
+        
+
+            2. Programar vuelos
+            Esta funcionalidad permite programar nuevos vuelos que se llevarán a cabo en el aeropuerto, para posteriormente
+            interactuar con ellos mediante otras funcionalidades, además de que implementa un sistema de recomendación de
+            empleados asignados al vuelo seguidamente de su creación, basándose en la experiencia del empleado.
+
+
+            3. Gestionar empleados
+            Esta funcionalidad imprime los múltiples empleados que trabajan en el aeropuerto, junto a su cédula para poder 
+            seleccionarlo y elegir entre opciones como cambiar de cargo, cambiar sueldo, asignar un vuelo y despedir empleados.
+            
+
+            4. Informacion sobre finanzas
+            Esta funcionalidad muestra el saldo actual del aeropuerto y reúne las siguientes opciones, pagar nomina, consultar
+            el historial de transacciones, contratar nuevos empleados e ingresar fondos al aeropuerto.
+
+
+            5. Modificaciones generales
+            Esta funcionalidad agrupa ciertas modificaciones posibles en dentro del sistema tales como cambiar de asiento de un
+            pasajero, cancelar un vuelo, eliminar un avion e ingresar nuevos aviones al aeropuerto.
+            '''
+
+            self.label = Label(self.ventanaOpera, text = descripcion, font = ("Courier", 10))
+            self.label.pack(ipadx=8, padx=8,ipady=8,pady=8,fill = X, anchor="w")
+            self.widgetsActuales.extend([self.lp, self.ld, self.label])
+
+        def reservaDeVuelo():
+            borrarElementos()
+            self.lp = Label(self.fp,text= "Reserva de vuelo", font = ("Courier", 12),height=2, bg="gray80")
+            self.lp.pack()
+            self.ld = Label(self.fd, text = "En este apartado puede realizar el agendamiento de un vuelo para un pasajero", font = ("Courier", 10))
+            self.ld.pack()
+            self.label = Label(self.ventanaOpera, text = "pruebita", font = ("Courier", 10))
+            self.label.pack(ipadx=8, padx=8,ipady=8,pady=8,fill = X, anchor="w")            
+            self.widgetsActuales.extend([self.lp, self.ld, self.label])
+
+        #Menus
         self._barraMenu = Menu(self)
 
         archivo = Menu(self._barraMenu)
+        self._barraMenu.add_cascade(label = "Archivo", menu = archivo)
         archivo.add_command(label = "Aplicacion", command = descripcion)
         archivo.add_command(label = "Salir y guardar", command = self.salir)
-        self._barraMenu.add_cascade(label = "Archivo", menu = archivo)
 
         procesosYConsultas = Menu(self._barraMenu)
         self._barraMenu.add_cascade(label="Procesos y consultas", menu=procesosYConsultas)
-        procesosYConsultas.add_command(label = "Funcionalidad 1", command = prueba)
-        procesosYConsultas.add_command(label = "Funcionalidad 2", command = prueba)
+        procesosYConsultas.add_command(label = "Funcionalidad 1", command = reservaDeVuelo)
+        procesosYConsultas.add_command(label = "Funcionalidad 2", command = pantallaPrincipal)
         procesosYConsultas.add_command(label = "Funcionalidad 3", command = prueba)
         procesosYConsultas.add_command(label = "Funcionalidad 4", command = prueba)
         procesosYConsultas.add_command(label = "Funcionalidad 5", command = prueba)
@@ -53,50 +117,9 @@ class VentanaUsuario(Tk):
         self.ventanaOpera = Frame(self.frame,bd=2)
         self.ventanaOpera.pack(padx = 5, pady= 5,fill=BOTH,expand = True)
 
-        descripcion = '''Bienvenidos a la aplicacion de gestion y modificacion de aeropuerto, aqui podra realizar diferentes funciones
-        relacionadas a la administracion del aeropuerto.
+        #Llamado a pantalla principal
+        pantallaPrincipal()
 
-        En la parte superior en el menu de procesos y consultas se listan Las principales funcionalidades, las cuales son:
-
-
-        1. Reserva de vuelo
-        En esta opcion esta disponible lo necesario para agendar un vuelo a determinado pasajero, se muestran en pantalla
-        todos los vuelos disponibles que se dirijan hacia la ciudad de destino deseada, luego se muestran los diferentes
-        vuelos disponibles. En el mismo apartado se implementa un pequeño formulario de datos personales para la
-        identificación del pasajero y así mismo la cantidad de equipajes que desea transportar. Tambien se le da la
-        posibilidad de elegir el asiento que desea, los cuales están separados por turista, ejecutiva y primera clase, 
-        esta elección influirá directamente en el costo del tiquete.
-        
-
-        2. Programar vuelos
-        Esta funcionalidad permite programar nuevos vuelos que se llevarán a cabo en el aeropuerto, para posteriormente
-        interactuar con ellos mediante otras funcionalidades, además de que implementa un sistema de recomendación de
-        empleados asignados al vuelo seguidamente de su creación, basándose en la experiencia del empleado.
-
-
-        3. Gestionar empleados
-        Esta funcionalidad imprime los múltiples empleados que trabajan en el aeropuerto, junto a su cédula para poder 
-        seleccionarlo y elegir entre opciones como cambiar de cargo, cambiar sueldo, asignar un vuelo y despedir empleados.
-        
-
-        4. Informacion sobre finanzas
-        Esta funcionalidad muestra el saldo actual del aeropuerto y reúne las siguientes opciones, pagar nomina, consultar
-        el historial de transacciones, contratar nuevos empleados e ingresar fondos al aeropuerto.
-
-
-        5. Modificaciones generales
-        Esta funcionalidad agrupa ciertas modificaciones posibles en dentro del sistema tales como cambiar de asiento de un
-        pasajero, cancelar un vuelo, eliminar un avion e ingresar nuevos aviones al aeropuerto.
-        '''
-
-        #Labels
-        self.lp = Label(self.fp,text= "Pantalla principal", font = ("Courier", 12),height=2, bg="gray80")
-        self.lp.pack()
-        self.ld = Label(self.fd, text = "Descripción de funcionalidades", font = ("Courier", 10))
-        self.ld.pack()
-        self.label = Label(self.ventanaOpera, text = descripcion, font = ("Courier", 10))
-        self.label.pack(ipadx=8, padx=8,ipady=8,pady=8,fill = X, anchor="w")
-    
     def salir(self):
         self.__class__.VSabierta = False
         self.ventanaInicio.iconify()
